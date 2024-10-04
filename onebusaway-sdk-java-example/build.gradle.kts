d13daae3a91bd4e0e79a73d4e0f24c588298ef9d
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-04 07:11:20 +0300

    feat: build gradle file for the examples

---
+++ onebusaway-sdk-java-example/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "0.1.0-alpha.4"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation(project(":onebusaway-sdk-java"))
+    implementation(project(mapOf("path" to ":onebusaway-sdk-java-core")))
+
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
