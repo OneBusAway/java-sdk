3dbd41db40868e688283a5c8c2742562ae9946f2
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2026-08-20 14:19:49 +0300

    Build SDK
    Stainless-Generated-From: 3cbf85918384ed6d827abc71fbd80f102ca81ef1

--- onebusaway-sdk-java-core/bin/main/org/onebusaway/errors/OnebusawaySdkIoException.kt
+++
@@ -1,6 +0,0 @@
-package org.onebusaway.errors
-
-class OnebusawaySdkIoException
-@JvmOverloads
-constructor(message: String? = null, cause: Throwable? = null) :
-    OnebusawaySdkException(message, cause)
