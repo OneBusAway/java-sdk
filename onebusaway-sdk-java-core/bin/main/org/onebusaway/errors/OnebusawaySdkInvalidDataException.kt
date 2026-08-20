6099d8f09a977e0168ebfa9062059ec528fc9bd2
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2026-08-20 14:40:58 +0300

    fix: untrack bin/ directories from git

--- onebusaway-sdk-java-core/bin/main/org/onebusaway/errors/OnebusawaySdkInvalidDataException.kt
+++
@@ -1,6 +0,0 @@
-package org.onebusaway.errors
-
-class OnebusawaySdkInvalidDataException
-@JvmOverloads
-constructor(message: String? = null, cause: Throwable? = null) :
-    OnebusawaySdkException(message, cause)
